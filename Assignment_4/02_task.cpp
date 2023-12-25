d0d1ec3a64ee85791a2bbc8afe2d968328ea52a7
NA-Siddiky <siddiky.academic@gmail.com>
2023-12-25 13:42:05 +0600

    solve task 02

---
+++ Assignment_4/02_task.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int multiply(int a, int b)
+{
+
+    if (a == 0 || b == 0)
+    {
+        return 0;
+    }
+    if (b == 1)
+    {
+        return a;
+    }
+
+    return a + multiply(a, b - 1);
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    cout << "Output: " << multiply(a, b) << endl;
+
+    return 0;
+}
