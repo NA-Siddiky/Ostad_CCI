69670d56111d0cb60a743cca2d278c5a76ca2d30
NA-Siddiky <siddiky.academic@gmail.com>
2024-01-01 22:50:52 +0600

    solved task 1

--- Assignment_5/01_task.cpp
+++ Assignment_5/01_task.cpp
@@ -1,28 +1,71 @@
 #include <iostream>
-
 using namespace std;
 
-int multiply(int a, int b)
+// Structure for a node in the linked list
+struct ListNode
 {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
 
-    if (a == 0 || b == 0)
+// Function to reverse the linked list iteratively
+ListNode *reverseList(ListNode *head)
+{
+    ListNode *prev = nullptr;
+    ListNode *current = head;
+    ListNode *next = nullptr;
+
+    while (current != nullptr)
     {
-        return 0;
-    }
-    if (b == 1)
-    {
-        return a;
+        next = current->next; // Store the next node
+        current->next = prev; // Reverse the link
+        prev = current;       // Update the previous pointer
+        current = next;       // Move to the next node
     }
 
-    return a + multiply(a, b - 1);
+    return prev; // The new head is the former tail
+}
+
+// Function to create a linked list with given values
+ListNode *createList(int arr[], int n)
+{
+    if (n == 0)
+    {
+        return nullptr;
+    }
+
+    ListNode *head = new ListNode(arr[0]);
+    ListNode *current = head;
+
+    for (int i = 1; i < n; i++)
+    {
+        current->next = new ListNode(arr[i]);
+        current = current->next;
+    }
+
+    return head;
+}
+
+// Function to print the linked list
+void printList(ListNode *head)
+{
+    while (head != nullptr)
+    {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
 }
 
 int main()
 {
-    int a, b;
-    cin >> a >> b;
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-    cout << "Output: " << multiply(a, b) << endl;
+    ListNode *head = createList(arr, n);
+    head = reverseList(head);
+    printList(head);
 
     return 0;
 }
